34458710223e8d6acecc92a3e0d768da0d4429dd
JiUnKang <46744270+rkdwldns00@users.noreply.github.com>
2022-12-01 13:01:46 +0900

    기본움직임까지 제작

--- Genshin/Assets/Control.cs
+++ Genshin/Assets/Control.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Control : MonoBehaviour
 {
     Animator animator;
+    public GameObject cam;
 
     void Start()
     {
@@ -14,7 +15,12 @@ public class Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.SetBool("isWalk", Input.GetAxis("Vertical") != 0f);
+        Debug.Log(Input.GetAxis("Vertical"));
+        animator.SetBool("isWalk", Input.GetAxis("Vertical") != 0f || Input.GetAxis("Horizontal") != 0f);
         animator.SetBool("isRun", Input.GetKey(KeyCode.LeftShift));
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0) {
+            transform.rotation = transform.rotation = Quaternion.Lerp(Quaternion.Euler(0, (Mathf.Atan2(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * Mathf.Rad2Deg) + 90f, 0), transform.rotation, 1-Time.deltaTime * 5);
+        }
+        cam.transform.position = transform.position;
     }
 }
