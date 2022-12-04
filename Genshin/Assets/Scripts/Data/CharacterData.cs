e65bd68e538c74bbf45f5a412e8f2e29b74e541b
JiUnKang <46744270+rkdwldns00@users.noreply.github.com>
2022-12-04 17:59:47 +0900

    폴더정리

---
+++ Genshin/Assets/Scripts/Data/CharacterData.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Character Data",menuName ="Scriptable Object/Character Data",order = int.MaxValue)]
+public class CharacterData : ScriptableObject
+{
+
+}
